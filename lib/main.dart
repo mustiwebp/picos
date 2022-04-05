4ade426f2011466729dd889fcdae051f5428ebea
mustafa_hits <mustafa.sezer@hit-solutions.de>
2022-04-05 11:00:18 +0200

    Change the setting of maximal number of characters within a line & the according adjustment in the dart-files

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,18 @@ Copyright 2022 by Mustafa Sezer <mustafa.sezer@hit-solutions.de>
 
 This file is part of picos.
 
-picos is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-Y; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.picos is distributed in the hope that it will be useful, but WITHOUT ANY WARRANT
-You should have received a copy of the GNU General Public License along with picos. If not, see <https://www.gnu.org/licenses/>.
+picos is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+picos is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along
+with picos. If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:flutter/material.dart';
