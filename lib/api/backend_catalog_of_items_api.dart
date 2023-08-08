4abc3eb430f18be507d8cc09505befa6af099496
mustiwebp <38396131+mustiwebp@users.noreply.github.com>
2023-08-08 16:47:12 +0200

    removeObject method implementation

--- lib/api/backend_catalog_of_items_api.dart
+++ lib/api/backend_catalog_of_items_api.dart
@@ -818,88 +818,391 @@ class BackendCatalogOfItemsApi extends BackendObjectsApi {
   @override
   Future<void> removeObject(AbstractDatabaseObject object) async {
     try {
-      ParseResponse icuDIagnoses = await Backend.getEntry(
+      ParseResponse icuDiagnosis = await Backend.getEntry(
         ICUDiagnosis.databaseTable,
         'Patient',
         object.objectId!,
       );
+
+      List<ICUDiagnosis> icuDiagnosisList = <ICUDiagnosis>[];
+
+      if (icuDiagnosis.results != null) {
+        for (dynamic element in icuDiagnosis.results!) {
+          icuDiagnosisList.add(
+            ICUDiagnosis(
+              mainDiagnosis: element['ICU_Hd'] ?? '',
+              ancillaryDiagnosis: element['Nebendiagnose'],
+              intensiveCareUnitAcquiredWeakness: element['ICU_AW'],
+              postIntensiveCareSyndrome: element['PICS'],
+              patientObjectId: element['Patient']['objectId'],
+              doctorObjectId: element['Doctor']['objectId'],
+              objectId: element['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
       ParseResponse bloodGasAnalysis = await Backend.getEntry(
         BloodGasAnalysis.databaseTable,
         'Patient',
         object.objectId!,
       );
+
+      List<BloodGasAnalysis> bloodGasAnalysisList = <BloodGasAnalysis>[];
+
+      String bloodGasAnalysisValue1 = '';
+      String bloodGasAnalysisValue2 = '';
+
+      if (bloodGasAnalysis.results != null) {
+        for (dynamic element in bloodGasAnalysis.results!) {
+          bloodGasAnalysisValue1 = element['value1']['objectId'];
+          bloodGasAnalysisValue2 = element['value2']['objectId'];
+
+          bloodGasAnalysisList.add(
+            BloodGasAnalysis(
+              valueObjectId1: element['value1']['objectId'],
+              valueObjectId2: element['value2']['objectId'],
+              patientObjectId: element['Patient']['objectId'],
+              doctorObjectId: element['Doctor']['objectId'],
+              objectId: element['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
+      ParseResponse bloodGasAnalysisObject1 = await Backend.getEntry(
+        BloodGasAnalysisObject.databaseTable,
+        'objectId',
+        bloodGasAnalysisValue1,
+      );
+
+      List<BloodGasAnalysisObject> bloodGasAnalysisObjectList1 =
+          <BloodGasAnalysisObject>[];
+
+      if (bloodGasAnalysisObject1.results != null) {
+        for (dynamic element in bloodGasAnalysisObject1.results!) {
+          bloodGasAnalysisObjectList1.add(
+            BloodGasAnalysisObject(
+              arterialOxygenSaturation: element['SaO2']?.toDouble(),
+              centralVenousOxygenSaturation: element['SzVO2']?.toDouble(),
+              partialPressureOfOxygen: element['PaO2_woTemp']?.toDouble(),
+              partialPressureOfCarbonDioxide:
+                  element['PaCO2_woTemp']?.toDouble(),
+              arterialBaseExcess: element['BE']?.toDouble(),
+              arterialPH: element['pH']?.toDouble(),
+              arterialSerumBicarbonateConcentration:
+                  element['Bicarbonat']?.toDouble(),
+              arterialLactate: element['Laktat']?.toDouble(),
+              bloodGlucoseLevel: element['BloodSugar']?.toDouble(),
+              objectId: element['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
+      ParseResponse bloodGasAnalysisObject2 = await Backend.getEntry(
+        BloodGasAnalysisObject.databaseTable,
+        'objectId',
+        bloodGasAnalysisValue2,
+      );
+
+      List<BloodGasAnalysisObject> bloodGasAnalysisObjectList2 =
+          <BloodGasAnalysisObject>[];
+
+      if (bloodGasAnalysisObject2.results != null) {
+        for (dynamic element in bloodGasAnalysisObject2.results!) {
+          bloodGasAnalysisObjectList2.add(
+            BloodGasAnalysisObject(
+              arterialOxygenSaturation: element['SaO2']?.toDouble(),
+              centralVenousOxygenSaturation: element['SzVO2']?.toDouble(),
+              partialPressureOfOxygen: element['PaO2_woTemp']?.toDouble(),
+              partialPressureOfCarbonDioxide:
+                  element['PaCO2_woTemp']?.toDouble(),
+              arterialBaseExcess: element['BE']?.toDouble(),
+              arterialPH: element['pH']?.toDouble(),
+              arterialSerumBicarbonateConcentration:
+                  element['Bicarbonat']?.toDouble(),
+              arterialLactate: element['Laktat']?.toDouble(),
+              bloodGlucoseLevel: element['BloodSugar']?.toDouble(),
+              objectId: element['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
       ParseResponse laborParameters = await Backend.getEntry(
         LaborParameters.databaseTable,
         'Patient',
         object.objectId!,
       );
+
+      List<LaborParameters> laborParametersList = <LaborParameters>[];
+
+      if (laborParameters.results != null) {
+        for (dynamic element in laborParameters.results!) {
+          laborParametersList.add(
+            LaborParameters(
+              leukocyteCount: element['Leukozyten']?.toDouble(),
+              lymphocyteCount: element['Lymphozyten_abs']?.toDouble(),
+              lymphocytePercentage: element['Lymphozyten_proz']?.toDouble(),
+              plateletCount: element['Thrombozyten']?.toDouble(),
+              cReactiveProteinLevel: element['CRP']?.toDouble(),
+              procalcitoninLevel: element['PCT']?.toDouble(),
+              interleukin: element['IL_6']?.toDouble(),
+              bloodUreaNitrogen: element['Urea']?.toDouble(),
+              creatinine: element['Kreatinin']?.toDouble(),
+              heartFailureMarker: element['BNP']?.toDouble(),
+              heartFailureMarkerNTProBNP: element['NT_Pro_BNP']?.toDouble(),
+              bilirubinTotal: element['Bilirubin']?.toDouble(),
+              hemoglobin: element['Haemoglobin']?.toDouble(),
+              hematocrit: element['Haematokrit']?.toDouble(),
+              albumin: element['Albumin']?.toDouble(),
+              gotASAT: element['GOT']?.toDouble(),
+              gptALAT: element['GPT']?.toDouble(),
+              troponin: element['Troponin']?.toDouble(),
+              creatineKinase: element['CK']?.toDouble(),
+              myocardialInfarctionMarkerCKMB: element['CK_MB']?.toDouble(),
+              lactateDehydrogenaseLevel: element['LDH']?.toDouble(),
+              amylaseLevel: element['Amylase']?.toDouble(),
+              lipaseLevel: element['Lipase']?.toDouble(),
+              dDimer: element['D_Dimere']?.toDouble(),
+              internationalNormalizedRatio: element['INR']?.toDouble(),
+              partialThromboplastinTime: element['pTT']?.toDouble(),
+              patientObjectId: element['Patient']['objectId'],
+              doctorObjectId: element['Doctor']['objectId'],
+              objectId: element['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
       ParseResponse movementData = await Backend.getEntry(
         PatientData.databaseTable,
         'Patient',
         object.objectId!,
       );
+
+      List<PatientData> movementDataList = <PatientData>[];
+
+      if (movementData.results != null) {
+        for (dynamic element in movementData.results!) {
+          movementDataList.add(
+            PatientData(
+              bodyHeight: element['BodyHeight']?.toDouble(),
+              patientID: element['ID'],
+              caseNumber: element['CaseNumber'],
+              instKey: element['inst_key'],
+              bodyWeight: element['BodyWeight']?.toDouble(),
+              ezpICU: element['EZP_ICU'],
+              birthDate: element['Birthdate'],
+              gender: element['Gender'],
+              bmi: element['BMI']?.toDouble(),
+              idealBMI: element['IdealBodyWeight']?.toDouble(),
+              azpICU: (element['AZP_ICU']),
+              ventilationDays: element['VentilationDays'],
+              azpKH: element['AZP_KH'],
+              ezpKH: element['EZP_KH'],
+              icd10Codes: element['ICD_10_Codes'],
+              station: element['Station'] ?? '',
+              lbgt70: element['LBgt70'],
+              icuLengthStay: element['ICU_LengthStay'],
+              khLengthStay: element['KH_LengthStay'],
+              wdaICU: element['WdaICU2'],
+              objectId: element['objectId'],
+              patientObjectId: element['Patient']['objectId'],
+              doctorObjectId: element['Doctor']['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
       ParseResponse vitalSigns = await Backend.getEntry(
         VitalSigns.databaseTable,
         'Patient',
         object.objectId!,
       );
+
+      List<VitalSigns> vitalSignsList = <VitalSigns>[];
+
+      String vitalSignsValue1 = '';
+      String vitalSignsValue2 = '';
+
+      if (vitalSigns.results != null) {
+        for (dynamic element in vitalSigns.results!) {
+          vitalSignsValue1 = element['value1']['objectId'];
+          vitalSignsValue2 = element['value2']['objectId'];
+
+          vitalSignsList.add(
+            VitalSigns(
+              valueObjectId1: element['value1']['objectId'],
+              valueObjectId2: element['value2']['objectId'],
+              objectId: element['objectId'],
+              patientObjectId: element['Patient']['objectId'],
+              doctorObjectId: element['Doctor']['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
+      ParseResponse vitalSignsObject1 = await Backend.getEntry(
+        VitalSignsObject.databaseTable,
+        'objectId',
+        vitalSignsValue1,
+      );
+
+      List<VitalSignsObject> vitalSignsObjectList1 = <VitalSignsObject>[];
+
+      if (vitalSignsObject1.results != null) {
+        for (dynamic element in vitalSignsObject1.results!) {
+          vitalSignsObjectList1.add(
+            VitalSignsObject(
+              heartRate: element['HeartRate']?.toDouble(),
+              systolicArterialPressure: element['SAP']?.toDouble(),
+              meanArterialPressure: element['MAP']?.toDouble(),
+              diastolicArterialPressure: element['DAP']?.toDouble(),
+              centralVenousPressure: element['ZVD']?.toDouble(),
+              objectId: element['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
+      ParseResponse vitalSignsObject2 = await Backend.getEntry(
+        VitalSignsObject.databaseTable,
+        'objectId',
+        vitalSignsValue2,
+      );
+
+      List<VitalSignsObject> vitalSignsObjectList2 = <VitalSignsObject>[];
+
+      if (vitalSignsObject2.results != null) {
+        for (dynamic element in vitalSignsObject2.results!) {
+          vitalSignsObjectList2.add(
+            VitalSignsObject(
+              heartRate: element['HeartRate']?.toDouble(),
+              systolicArterialPressure: element['SAP']?.toDouble(),
+              meanArterialPressure: element['MAP']?.toDouble(),
+              diastolicArterialPressure: element['DAP']?.toDouble(),
+              centralVenousPressure: element['ZVD']?.toDouble(),
+              objectId: element['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
       ParseResponse respiratoryParameters = await Backend.getEntry(
         RespiratoryParameters.databaseTable,
         'Patient',
         object.objectId!,
       );
 
-      ParseResponse bloodGasAnalysisObject1 = await Backend.getEntry(
-        BloodGasAnalysisObject.databaseTable,
-        'objectId',
-        (bloodGasAnalysis.results?[0] as BloodGasAnalysis).valueObjectId1!,
-      );
-      ParseResponse bloodGasAnalysisObject2 = await Backend.getEntry(
-        BloodGasAnalysisObject.databaseTable,
-        'objectId',
-        (bloodGasAnalysis.results?[0] as BloodGasAnalysis).valueObjectId2!,
-      );
-      ParseResponse respiratoryParametersObject1 = await Backend.getEntry(
-        RespiratoryParametersObject.databaseTable,
-        'objectId',
-        (respiratoryParameters.results?[0] as RespiratoryParameters)
-            .valueObjectId1!,
-      );
-      ParseResponse respiratoryParametersObject2 = await Backend.getEntry(
-        RespiratoryParametersObject.databaseTable,
-        'objectId',
-        (respiratoryParameters.results?[0] as RespiratoryParameters)
-            .valueObjectId2!,
-      );
-      ParseResponse vitalSignsObject1 = await Backend.getEntry(
+      List<RespiratoryParameters> respiratoryParametersList =
+          <RespiratoryParameters>[];
+
+      String respiratoryParametersValue1 = '';
+      String respiratoryParametersValue2 = '';
+
+      if (respiratoryParameters.results != null) {
+        for (dynamic element in respiratoryParameters.results!) {
+          respiratoryParametersValue1 = element['value1']['objectId'];
+          respiratoryParametersValue2 = element['value2']['objectId'];
+
+          respiratoryParametersList.add(
+            RespiratoryParameters(
+              valueObjectId1: element['value1']['objectId'],
+              valueObjectId2: element['value2']['objectId'],
+              objectId: element['objectId'],
+              patientObjectId: element['Patient']['objectId'],
+              doctorObjectId: element['Doctor']['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
+      ParseResponse respiratoryParamtersObject1 = await Backend.getEntry(
         VitalSignsObject.databaseTable,
         'objectId',
-        (vitalSigns.results?[0] as VitalSigns).valueObjectId1!,
-      );
-      ParseResponse vitalSignsObject2 = await Backend.getEntry(
-        VitalSignsObject.databaseTable,
-        'objectId',
-        (vitalSigns.results?[0] as VitalSigns).valueObjectId2!,
+        respiratoryParametersValue1,
       );
 
-      await Backend.removeObject(
-        bloodGasAnalysis.result,
+      List<RespiratoryParametersObject> respiratoryParametersObjectList1 =
+          <RespiratoryParametersObject>[];
+
+      if (respiratoryParamtersObject1.results != null) {
+        for (dynamic element in respiratoryParamtersObject1.results!) {
+          respiratoryParametersObjectList1.add(
+            RespiratoryParametersObject(
+              tidalVolume: element['VT']?.toDouble(),
+              respiratoryRate: element['AF']?.toDouble(),
+              oxygenSaturation: element['SpO2']?.toDouble(),
+              objectId: element['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
+      ParseResponse respiratoryParamtersObject2 = await Backend.getEntry(
+        VitalSignsObject.databaseTable,
+        'objectId',
+        respiratoryParametersValue2,
       );
-      await Backend.removeObject(bloodGasAnalysisObject1.result);
-      await Backend.removeObject(bloodGasAnalysisObject2.result);
-      await Backend.removeObject(icuDIagnoses.result);
-      await Backend.removeObject(laborParameters.result);
-      await Backend.removeObject(movementData.result);
-      await Backend.removeObject(respiratoryParameters.result);
-      await Backend.removeObject(respiratoryParametersObject1.result);
-      await Backend.removeObject(respiratoryParametersObject2.result);
-      await Backend.removeObject(vitalSigns.result);
-      await Backend.removeObject(vitalSignsObject1.result);
-      await Backend.removeObject(vitalSignsObject2.result);
+
+      List<RespiratoryParametersObject> respiratoryParametersObjectList2 =
+          <RespiratoryParametersObject>[];
+
+      if (respiratoryParamtersObject2.results != null) {
+        for (dynamic element in respiratoryParamtersObject2.results!) {
+          respiratoryParametersObjectList2.add(
+            RespiratoryParametersObject(
+              tidalVolume: element['VT']?.toDouble(),
+              respiratoryRate: element['AF']?.toDouble(),
+              oxygenSaturation: element['SpO2']?.toDouble(),
+              objectId: element['objectId'],
+              createdAt: element['createdAt'],
+              updatedAt: element['updatedAt'],
+            ),
+          );
+        }
+      }
+
+      await Backend.removeObject(bloodGasAnalysisList.first);
+      await Backend.removeObject(bloodGasAnalysisObjectList1.first);
+      await Backend.removeObject(bloodGasAnalysisObjectList2.first);
+      await Backend.removeObject(icuDiagnosisList.first);
+      await Backend.removeObject(laborParametersList.first);
+      await Backend.removeObject(movementDataList.first);
+      await Backend.removeObject(respiratoryParametersList.first);
+      await Backend.removeObject(respiratoryParametersObjectList1.first);
+      await Backend.removeObject(respiratoryParametersObjectList2.first);
+      await Backend.removeObject(vitalSignsList.first);
+      await Backend.removeObject(vitalSignsObjectList1.first);
+      await Backend.removeObject(vitalSignsObjectList2.first);
 
       dispatch();
     } catch (e) {
-      return;
+      Stream<List<CatalogOfItemsElement>>.error(e);
     }
   }
 }
